0e489cd815895f16040c7d468d908b19b4faa090
drxvill <drxvill@yahoo.com>
2024-08-01 19:36:24 +0100

    updated sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,20 +2,29 @@
 </script>
 
 <template>
-    <UCard class="fixed hidden md:flex flex-col justify-center items-center w-16 h-screen font-outfit select-none">
-        <div class="space-y-4">
-            <UTooltip text="Home" :popper="{ placement: 'right' }">
-                <UButton to="/" icon="i-heroicons-home-solid" color="gray" variant="ghost" size="xl" block />
-            </UTooltip>
-            <UTooltip text="Explore" :popper="{ placement: 'right' }">
-                <UButton to="/explore" icon="i-heroicons-fire" color="gray" variant="ghost" size="xl" block />
-            </UTooltip>
-            <UTooltip text="Bookmarks" :popper="{ placement: 'right' }">
-                <UButton to="/bookmarks" icon="i-heroicons-bookmark" color="gray" variant="ghost" size="xl" block />
-            </UTooltip>
-            <UTooltip text="Random" :popper="{ placement: 'right' }">
-                <UButton to="/random" icon="i-heroicons-bolt" color="gray" variant="ghost" size="xl" block />
-            </UTooltip>
+    <div class="fixed hidden md:flex">
+        <div class="flex flex-col w-16 h-screen font-outfit select-none p-2">
+            <div class="flex flex-col gap-4">
+                <UTooltip text="Home" :popper="{ placement: 'right' }">
+                    <UButton to="/" icon="i-heroicons-home-solid" color="gray" variant="ghost" size="xl" block />
+                </UTooltip>
+                <UTooltip text="Explore" :popper="{ placement: 'right' }">
+                    <UButton to="/explore" icon="i-heroicons-fire-solid" color="gray" variant="ghost" size="xl" block />
+                </UTooltip>
+                <UTooltip text="Bookmarks" :popper="{ placement: 'right' }">
+                    <UButton to="/bookmarks" icon="i-heroicons-bookmark-solid" color="gray" variant="ghost" size="xl"
+                        block />
+                </UTooltip>
+                <UTooltip text="Random" :popper="{ placement: 'right' }">
+                    <UButton to="/random" icon="i-heroicons-bolt-solid" color="gray" variant="ghost" size="xl" block />
+                </UTooltip>
+            </div>
+            <div class="flex flex-col gap-4 mt-auto">
+                <UTooltip text="Settings" :popper="{ placement: 'right' }">
+                    <UButton to="/settings" icon="i-heroicons-cog-6-tooth-16-solid" color="gray" variant="ghost"
+                        size="xl" block />
+                </UTooltip>
+            </div>
         </div>
-    </UCard>
+    </div>
 </template>
