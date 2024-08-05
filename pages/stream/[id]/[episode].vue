0f8f4ae017d45132edd0b224ae256d2d945530cf
drxvill <drxvill@yahoo.com>
2024-08-05 16:53:28 +0100

    updated media info "added sub or dub availability button"

--- pages/stream/[id]/[episode].vue
+++ pages/stream/[id]/[episode].vue
@@ -91,6 +91,9 @@ const items = [
         <div class="hidden lg:flex flex-col gap-2">
             <NuxtImg :src="data?.info.cover" :alt="data?.info.title" format="webp" quality="80" sizes="100vw"
                 placeholder width="100" height="100" class="w-56 h-80 rounded-md object-cover" />
+            <UButton :label="data?.info.dub === true ? 'Sub & Dub' : 'Only Sub'"
+                :color="data?.info.dub === true ? 'indigo' : 'teal'" variant="soft" size="lg" block
+                v-if="data?.info.dub !== null" />
             <UButton icon="i-heroicons-bookmark-solid" label="Bookmarked" variant="soft" size="lg" block
                 @click="onRemoveBookmark" v-if="isBookmarked()" />
             <UButton icon="i-heroicons-bookmark" label="Bookmark" variant="ghost" size="lg" block @click="onAddBookmark"
@@ -103,6 +106,9 @@ const items = [
                 <p class="text-base font-normal">{{ data?.info.season }} {{ data?.info.year }}</p>
                 <p class="text-2xl font-bold line-clamp-3">{{ data?.info.title }}</p>
             </div>
+            <UButton :label="data?.info.dub === true ? 'Sub & Dub' : 'Only Sub'"
+                :color="data?.info.dub === true ? 'indigo' : 'teal'" variant="soft" size="lg" block
+                v-if="data?.info.dub !== null" />
             <UButton icon="i-heroicons-bookmark-solid" label="Bookmarked" variant="soft" size="lg" block
                 @click="onRemoveBookmark" v-if="isBookmarked()" />
             <UButton icon="i-heroicons-bookmark" label="Bookmark" variant="ghost" size="lg" block @click="onAddBookmark"
